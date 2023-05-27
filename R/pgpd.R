6fb8714cf81f74b5b78dee5d4d6ef4eab024828e
paulaoak <123162329+paulaoak@users.noreply.github.com>
2023-05-27 10:07:17 +0100

    Update pgpd.R
    allow quantiles vector, shape, scale and shift parameter vector to be of different lengths and output warning message in that case

--- R/pgpd.R
+++ R/pgpd.R
@@ -24,27 +24,24 @@ pgpd <- function(q, scale = 1, shape = 0, shift = 0, shape_tolerance = 1e-10){
   n <- max(input_lengths)
   stopifnot(exprs = {
     all(scale > 0)
-    length(q) %in% c(1,n)
-    length(scale) %in% c(1,n)
-    length(shape) %in% c(1,n)
-    length(shift) %in% c(1,n)
     length(shape_tolerance) == 1
     shape_tolerance >= 0
   })
 
   # Ensure q, scale, shape and mu are of same length.
-  if ((length(scale) == 1) & (n > 1)) { scale <- rep(scale, n) }
-  if ((length(shape) == 1) & (n > 1)) { shape <- rep(shape, n) }
-  if ((length(shift) == 1) & (n > 1)) { shift <- rep(shift, n) }
-  if ((length(q) == 1) & (n > 1)) {q <- rep(q, n)}
+  if ((length(scale) < n) & (n > 1)) { scale <- rep(scale, length.out = n) }
+  if ((length(shape) < n) & (n > 1)) { shape <- rep(shape, length.out = n) }
+  if ((length(shift) < n) & (n > 1)) { shift <- rep(shift, length.out = n) }
+  if ((length(q) < n) & (n > 1)) {q <- rep(q, length.out = n)}
 
   # Calculate probabilities
   p <- (1 - (1 + (shape * (q - shift)) / scale)^(-1 / shape))
 
-  # Correct probabilities below threshold or above upper end point
+  # Correct probabilities below threshold
   value_below_threshold <- 0
   p[q < shift] <- value_below_threshold
 
+  # Correct probabilities above upper end point
   value_above_UEP <- 1
   is_above_UEP <- (shape < 0) & (q >= (shift - scale / shape))
   p[is_above_UEP] <- value_above_UEP
@@ -60,6 +57,11 @@ pgpd <- function(q, scale = 1, shape = 0, shift = 0, shape_tolerance = 1e-10){
     exp_ps <- stats::pexp(q = exp_qs, rate = exp_rates)
     p[which_shape_near_zero] <- exp_ps
   }
+  
+  # Show warning if quantiles, shape, scale and shift inputs are not of the same length
+  show_warning = sum(input_lengths) < 4*n
+  warning('Quantile vector, scale, shape and shift parameter vectors are not of the same length; shorter vectors are recycled', call. = show_warning, immediate. = FALSE, noBreaks. = FALSE,
+          domain = NULL)
 
   return(p)
 }
