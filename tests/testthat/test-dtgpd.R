8912e32b4fdc33cd0f50746b0165e150036446e3
paulaoak <pc2222@ic.ac.uk>
2023-06-05 15:11:04 +0100

    correct dtgpd test file

--- tests/testthat/test-dtgpd.R
+++ tests/testthat/test-dtgpd.R
@@ -14,7 +14,7 @@ test_that("exponential case works",{
 
 test_that("linear case works", {
   evaluation_points <- seq(-1, 4)
-  dtgpd_values <- dgpd(x = evaluation_points, scale = 2, shape = -0.5, scale_taper = 4)
+  dtgpd_values <- dtgpd(x = evaluation_points, scale = 2, shape = -0.5, scale_taper = 4)
   empirical_values <- (0.25 + 0.5 * pmax(1 - 0.25 * evaluation_points, 0)^(-1)) *exp(evaluation_points/4) * pmax(1 - 0.25 * evaluation_points, 0)^2 * (evaluation_points >= 0)
   expect_equal(dtgpd_values, empirical_values)
 })
