674d078a141872e432e07e1b410ba2532f3bc9ee
paulaoak <pc2222@ic.ac.uk>
2023-07-08 11:10:31 +0100

    test rgpd_mmax_mean function

--- tests/testthat/test-rgpd_mmax_mean.R
+++ tests/testthat/test-rgpd_mmax_mean.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("examples work", {
+  expect_no_condition(rgpd_mmax_mean(n = 5, mmax = 3, mean = 1, shift = 0))
+  expect_no_condition(rgpd_mmax_mean(n = 5, mmax = 3:5, mean = 1, shift = 0))
+  expect_no_condition(rgpd_mmax_mean(n = 5, mmax = 10, shape = 0.5 * 1:5, shift = 0))
+  expect_no_condition(rgpd_mmax_mean(n = 5, scale = 10, shape = 5, shift = 1:4))
+})
+
+test_that("shift greater than mean or mean greater than mmax scale parameters are flagged", {
+  expect_error(rgpd_mmax_mean(n = 5, mmax = 0.5, mean = 1, shift = 0))
+  expect_error(rgpd_mmax_mean(n = 5, mmax = 3, mean = 1, shift = 2))
+  expect_error(rgpd_mmax_mean(n = 5, mmax = c(1,0), mean = 1, shift = 0))
+})
+
+test_that("setting the seed works", {
+  set.seed(1234)
+  gpd_sample_2 <-rgpd_mmax_mean(n = 5, mmax = 3, mean = 1, shift = 0)
+  set.seed(1234)
+  gpd_sample_1 <- rgpd_mmax_mean(n = 5, mmax = 3, mean = 1, shift = 0)
+  expect_equal(gpd_sample_1, gpd_sample_2)
 })
